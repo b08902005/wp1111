2422270a8488c1eaf77da05d8e1187aa65b902b8
b08902005 <b08902005@ntu.edu.tw>
2022-12-20 01:39:12 +0000

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -1,6 +1,13 @@
-import { useQuery } from '@apollo/client';
+import { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 
 import { GET_ITEMS_QUERY } from '../graphql/queries';
+import { UPDATE_ITEM_MUTATION, DELETE_ITEM_MUTATION } from '../graphql/mutations';
+import {
+  ITEM_CREATED_SUBSCRIPTION,
+  ITEM_UPDATED_SUBSCRIPTION,
+  ITEM_DELETED_SUBSCRIPTION,
+} from '../graphql/subscriptions';
 
 import Balance from './Balance';
 import Category from './Category';
@@ -10,13 +17,54 @@ function Analytics() {
   const {
     loading, error, data: itemsData, subscribeToMore,
   } = useQuery(GET_ITEMS_QUERY);
+
+  useEffect(
+    () => {
+      subscribeToMore({
+        document: ITEM_CREATED_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev;
+          const item = subscriptionData.data.itemCreated;
+          return {
+            items: [item, ...prev.items],
+          };
+        },
+      });
+    },
+    [subscribeToMore],
+  );
+
+  useEffect(
+    () => {
+      subscribeToMore({
+        document: ITEM_UPDATED_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev;
+          const updatedItem = subscriptionData.data.itemUpdated;
+          return {
+            items: prev.items.map((item) => (item.id === updatedItem.id ? updatedItem : item)),
+          };
+        },
+      });
+    },
+    [subscribeToMore],
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return (<p>Error :(</p>);
+  }
+
+  
   const { items } = itemsData;
   // TODO 2.2 End
 
 
   return (
     <div className="grid grid-cols-12 gap-6">
-      { 
+      {
         // TODO 2.3 Add Balence and Category (uncomment the following code)
       }
       <div className="col-span-6">
